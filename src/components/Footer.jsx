086ceb3cced1a9bb6a5e988617b71a3602c26baf
Adarsh <124388283+exorcist09@users.noreply.github.com>
2024-01-26 23:04:17 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import {
   FaFacebookF,
   FaInstagram,
@@ -26,18 +27,18 @@ const Footer = () => {
         </ul>
         <div className="infoText">StreamPod, an avant-garde Over-the-Top (OTT) platform revolutionizing entertainment. Seamlessly delivering high-quality content to smartphones, tablets and computers,  Its sophisticated algorithms analyze user preferences, offering personalized recommendations. The platform boasts exclusive movies, series, and documentaries. With a subscription-based model, affordability meets value, complemented by optional offline viewing. Social integration fosters a connected community, while global reach and localization features ensure a diverse and inclusive entertainment experience. StreamPod stands as a beacon of innovation in the dynamic world of OTT streaming, where entertainment meets excellence.</div>
         <div className="socialIcons">
-          <span className="icon">
+          <Link className="icon" to="https://github.com/exorcist09">
             <FaGithub />
-          </span>
-          <span className="icon">
+          </Link>
+          <Link className="icon" to="https://www.linkedin.com/in/adarsh-verma-exorcist09">
             <FaLinkedin />
-          </span>
-          <span className="icon">
+          </Link>
+          <Link className="icon" to="">
           <FaXTwitter />
-          </span>
-          <span className="icon">
+          </Link>
+          <Link className="icon">
           <IoPersonCircleOutline />
-          </span>
+          </Link>
         </div>
       </ContentWrapper>
     </footer>
